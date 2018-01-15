0f7b4dacd000720f0f54c4185a300442e3e1abf1
ervinpepic <ervin.hack@gmail.com>
2018-01-15 15:16:55 +0100

    Initial Commit

---
+++ firchatos/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  firchatos
+//
+//  Created by Ervin on 15/01/2018.
+//  Copyright © 2018 Ervin Pepic. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
