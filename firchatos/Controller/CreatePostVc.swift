f0d0ecf519657aa19f5c6f10c7207a3b279fa59a
ervinpepic <ervin.hack@gmail.com>
2018-01-18 16:18:59 +0100

    Added posts VC where user can create post and save it on firebase

---
+++ firchatos/Controller/CreatePostVc.swift
@@ -0,0 +1,54 @@
+//
+//  CreatePostVc.swift
+//  firchatos
+//
+//  Created by Ervin on 18/01/2018.
+//  Copyright © 2018 Ervin Pepic. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVc: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var textField: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textField.delegate = self
+
+        
+    }
+//Actions
+    @IBAction func sendbuttopressed(_ sender: Any) {
+        if textField.text != nil && textField.text != "Say something" {
+            sendButton.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textField.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendButton.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                    print("success!!")
+                } else {
+                    self.sendButton.isEnabled = true
+                    print("There was an error")
+                }
+            })
+        }
+        
+    }
+   
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension CreatePostVc: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textField.text = ""
+    }
+}
