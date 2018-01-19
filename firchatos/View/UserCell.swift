25231225cdd61c7f6643d06b4e3e2bdb33eb862e
ervinpepic <ervin.hack@gmail.com>
2018-01-19 21:25:18 +0100

    Added new Groups view control and ability to create group

---
+++ firchatos/View/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  firchatos
+//
+//  Created by Ervin on 19/01/2018.
+//  Copyright © 2018 Ervin Pepic. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
