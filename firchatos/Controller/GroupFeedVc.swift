ddbe2c1630b9e92dac8f2f16beb254999253fdfe
ervinpepic <ervin.hack@gmail.com>
2018-01-25 00:58:48 +0100

    Added ability to get all messages from firebase & bug fixses

--- firchatos/Controller/GroupFeedVc.swift
+++ firchatos/Controller/GroupFeedVc.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class GroupFeedVc: UIViewController {
     
@@ -20,7 +21,9 @@ class GroupFeedVc: UIViewController {
     
     
     //vars
+    
     var group: Group?
+    var groupMessages = [Messages]()
     //Functions
     
     func initGroupData(forGroup group: Group) {
@@ -30,6 +33,8 @@ class GroupFeedVc: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         sendButtonView.bindToKeyboard()
+        tableView.delegate = self
+        tableView.dataSource = self
     
     }
     
@@ -40,13 +45,32 @@ class GroupFeedVc: UIViewController {
             self.members.text = returnedEmails.joined(separator: ", ")
         }
         
-        
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllMessagesFor(desiredGroup: self.group!, handler: { (returnedGroupMessages) in
+                self.groupMessages = returnedGroupMessages
+                self.tableView.reloadData()
+                
+                if self.groupMessages.count > 0 {
+                    self.tableView.scrollToRow(at: IndexPath(row: self.groupMessages.count - 1, section: 0), at: .none, animated: true)
+                }
+            })
+        }
     }
     
     //Actions
     
     @IBAction func sendButtonPressed(_ sender: Any) {
-        
+        if messageText.text != "" {
+            messageText.isEnabled = false
+            sendButton.isEnabled = false
+            DataService.instance.uploadPost(withMessage: messageText.text!, forUID: Auth.auth().currentUser!.uid, withGroupKey: group?.key, sendComplete: { (complete) in
+                if complete {
+                    self.messageText.text = ""
+                    self.messageText.isEnabled = true
+                    self.sendButton.isEnabled = true
+                }
+            })
+        }
     }
     
     @IBAction func backButton(_ sender: Any) {
@@ -54,3 +78,25 @@ class GroupFeedVc: UIViewController {
     }
     
 }
+
+extension GroupFeedVc: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groupMessages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupfeedCell", for: indexPath) as? GroupFeedCell else { return UITableViewCell() }
+        let message = groupMessages[indexPath.row]
+        DataService.instance.getUsername(forUid: message.senderId) { (returnedEmail) in
+            cell.configureCell(profileImage: UIImage(named: "defaultProfileImage")!, email: returnedEmail, content: message.content)
+            
+        }
+        
+        return cell
+    }
+}
+
--- firchatos/Services/DataService.swift
+++ firchatos/Services/DataService.swift
@@ -51,7 +51,8 @@ class DataService {
     func uploadPost(withMessage message: String, forUID uid: String, withGroupKey groupKey: String?,sendComplete: @escaping (_ status: Bool) -> ()) {
         
         if groupKey != nil {
-            //send to groups referencce
+            REF_GROUPS.child(groupKey!).child("messages").childByAutoId().updateChildValues(["content": message, "senderId": uid])
+            sendComplete(true)
         } else {
             REF_FEED.childByAutoId().updateChildValues(["content": message, "senderId": uid])
             sendComplete(true)
@@ -71,6 +72,24 @@ class DataService {
             }
             handler(messageArray)
         }
+    }
+    
+    
+    func getAllMessagesFor(desiredGroup: Group, handler: @escaping(_ messagesArray: [Messages]) -> ()) {
+            var groupMessageArray = [Messages]()
+            REF_GROUPS.child(desiredGroup.key).child("messages").observeSingleEvent(of: .value) { (groupMessageSnapshot) in
+                guard let groupMessageSnapshot = groupMessageSnapshot.children.allObjects as? [DataSnapshot] else { return }
+                for groupMessage in groupMessageSnapshot {
+                    let content = groupMessage.childSnapshot(forPath: "content").value as! String
+                    let senderId = groupMessage.childSnapshot(forPath: "senderId").value as! String
+                    let groupMessage = Messages(content: content, senderId: senderId)
+                    groupMessageArray.append(groupMessage)
+                }
+                handler(groupMessageArray)
+                
+               
+            }
+        
     }
     
     func getUsername(forUid uid: String, handler: @escaping (_ username: String) -> ()) {
